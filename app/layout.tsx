ee6d6ab757dfbdf6975489756612a91f97b3c3ad
waelkamira <ramondm4@gmail.com>
2024-03-25 19:05:12 +0300

    new start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import { Nunito } from 'next/font/google';
-import Navbar from './components/navbar/Navbar';
-import ClientOnly from './components/ClientOnly';
-import Modal from './components/modal/Modal';
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -22,13 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>
-        <ClientOnly>
-          <Modal isOpen title="Hello World" />
-          <Navbar />
-        </ClientOnly>
-        {children}
-      </body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
